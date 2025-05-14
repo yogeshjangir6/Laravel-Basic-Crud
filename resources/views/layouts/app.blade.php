8910510c545fbffc41ab0c89f6a5eb9553ee9ab0
Yogesh Jangir <yogeshjangir2001@gmail.com>
2025-05-14 07:30:52 +0530

    success/error msg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,21 @@
 </head>
 
 <body>
+    @if (session('success'))
+        <div style="color:green">
+            {{ session('success') }}
+        </div>
+    @endif
+    @if ($errors->any())
+        <div style="color:red">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>   
+        </div>
+    @endif
+    <h1>Laravel CRUD Application</h1> 
     @yield('content')
     <br/>
 </body>
