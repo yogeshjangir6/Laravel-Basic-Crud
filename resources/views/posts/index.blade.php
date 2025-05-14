f3c357b8ca1aaa23ba1a81641be22bd4a76ffeb6
Yogesh Jangir <yogeshjangir2001@gmail.com>
2025-05-14 06:29:40 +0530

    Post Index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>All Post</title>
+</head>
+
+<body>
+    <h1>Posts</h1>
+    <a href="{{ route('posts.create') }}">Create New Post</a>
+
+    <ul>
+        @foreach ($posts as $post)
+           <li><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
